8aa4a6b92edf63a63e864277088b2cbae464e500
Tammy Kanashiro <tammy.kanashiro@aligent.com.au>
2024-08-30 11:13:47 +0930

    Add grid card to Css category

--- src/pages/Css/index.tsx
+++ src/pages/Css/index.tsx
@@ -1,5 +1,15 @@
+import { Fragment } from "react/jsx-runtime";
+import Card from "../../components/Card";
+import gridImage from "../../images/grid.png";
+
 const Css = () => {
-    return <h1>Css</h1>;
+    return (
+        <Fragment>
+            <h1>Css</h1>
+
+            <Card title="Grid" link="/css/grid" image={gridImage} />
+        </Fragment>
+    );
 };
 
 export default Css;
