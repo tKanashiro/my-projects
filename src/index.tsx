c9a3ae23bd41aac2f83099815d66835b23955be8
Tammy Kanashiro <tammy.kanashiro@aligent.com.au>
2024-08-30 09:51:08 +0930

    Update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+import { ApolloProvider } from "@apollo/client";
 import Home from "./pages/Home";
 import client from "./apollo-client";
-import { ApolloProvider } from "@apollo/client";
+import Header from "./components/Header";
+import Layout from "./components/Layout/Layout";
+import "./index.css";
+import "./global/_styles.scss";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <ApolloProvider client={client}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                </Routes>
-            </BrowserRouter>
-        </ApolloProvider>
+        <Layout>
+            <Fragment>
+                <Header />
+                <ApolloProvider client={client}>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                        </Routes>
+                    </BrowserRouter>
+                </ApolloProvider>
+            </Fragment>
+        </Layout>
     </React.StrictMode>
 );
