b4c17fa7a5f68d81feade784702300caa27e8109
Tammy Kanashiro <tammy.kanashiro@aligent.com.au>
2024-09-02 14:43:07 +0930

    Add grid example

--- src/pages/Css/Grid/index.tsx
+++ src/pages/Css/Grid/index.tsx
@@ -1,7 +1,49 @@
 import Title from "components/Title";
+import { Fragment } from "react/jsx-runtime";
+import "./Grid.scss";
 
 const Grid = () => {
-    return <Title title="Grid" />;
+    return (
+        <Fragment>
+            <Title title="Grid" />
+
+            <div className="parent">
+                <div className="child">1</div>
+                {/* <div className="child">1</div>
+                <div className="child">2</div> */}
+
+                {/* <div className="child">3</div>
+                <div className="child">4</div> */}
+
+                {/*<div className="child">5</div>
+                <div className="child">6</div>
+                <div className="child">7</div>
+                <div className="child">8</div>
+                <div className="child">9</div>
+                <div className="child">10</div>
+                <div className="child">11</div>
+                <div className="child">12</div>
+                <div className="child">13</div>
+                <div className="child">14</div>
+                <div className="child">15</div>
+                <div className="child">16</div>
+                <div className="child">17</div>
+                <div className="child">18</div>
+                <div className="child">19</div>
+                <div className="child">20</div>
+                <div className="child">21</div>
+                <div className="child">22</div>
+                <div className="child">23</div>
+                <div className="child">24</div>
+                <div className="child">25</div>
+                <div className="child">26</div>
+                <div className="child">27</div>
+                <div className="child">28</div>
+                <div className="child">29</div>
+                <div className="child">30</div> */}
+            </div>
+        </Fragment>
+    );
 };
 
 export default Grid;
