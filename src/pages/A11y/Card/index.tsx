bd1709e949ee9e78a309d90d493431d5b83cc5e2
Tammy Kanashiro <tammy.kanashiro@aligent.com.au>
2025-02-17 10:23:47 +1030

    A11y card

--- src/pages/A11y/Card/index.tsx
+++ src/pages/A11y/Card/index.tsx
@@ -1,4 +1,9 @@
-import "./Card.scss";
+import "./CardA11y.scss";
+import image1 from "./images/image1.jpg";
+import image2 from "./images/image2.jpg";
+import image3 from "./images/image3.jpg";
+import image4 from "./images/image4.jpg";
+import image5 from "./images/image5.jpg";
 
 type CardDataType = {
     image: string;
@@ -9,7 +14,7 @@ type CardDataType = {
 
 const data = [
     {
-        image: "/images/image1.jpg",
+        image: image1,
         title: "Lorem ipsum photo",
         description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
             Eius nostrum deserunt itaque nisi fugiat odio est 
@@ -18,20 +23,20 @@ const data = [
         author: "Author A",
     },
     {
-        image: "/images/image1.jpg",
+        image: image2,
         title: "Lorem ipsum dolor, sit amet consectetur photo",
         description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
             Eius nostrum deserunt itaque nisi fugiat odio est.`,
         author: "Author B",
     },
     {
-        image: "/images/image1.jpg",
+        image: image3,
         title: "Lorem ipsum photo",
         description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit.`,
         author: "Author C",
     },
     {
-        image: "/images/image1.jpg",
+        image: image4,
         title: "Lorem ipsum dolor, sit amet photo",
         description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
             Eius nostrum deserunt itaque nisi fugiat odio est 
@@ -39,7 +44,7 @@ const data = [
         author: "Author D",
     },
     {
-        image: "/images/image1.jpg",
+        image: image5,
         title: "Lorem photo",
         description: `Lorem ipsum dolor.`,
         author: "Author E",
@@ -47,16 +52,59 @@ const data = [
 ];
 
 const cardContent = (info: CardDataType) => {
-    console.log({ info });
+    const { author, description, image, title } = info;
+
     return (
-        <li key={info.title}>
-            <p>{info.title}</p>
+        <li key={title} className="cardA11y">
+            <div className="imageWrapper">
+                <img src={image} alt="" />
+            </div>
+
+            <div className="a11yBody">
+                <h2>
+                    <a
+                        aria-describedby="desc-card-design"
+                        className="a11yLink"
+                        href="/a11y/card"
+                    >
+                        {title}
+                    </a>
+                </h2>
+                <p>{description}</p>
+                <span
+                    className="readoMore"
+                    aria-hidden="true"
+                    id="desc-card-design"
+                >
+                    read more
+                </span>
+                <small>{author}</small>
+            </div>
         </li>
     );
 };
 
 const Card = () => {
-    return <ul>{data.map((cardDetails) => cardContent(cardDetails))}</ul>;
+    return (
+        <>
+            <a href="https://inclusive-components.design/cards/">
+                <h1 className="a11yTitle">Card</h1>
+            </a>
+
+            <ul className="cardsWrapper">
+                {data.map((cardDetails) => cardContent(cardDetails))}
+            </ul>
+
+            <div className="keyPoints">
+                <p>
+                    <b>Card link:</b>{" "}
+                    <a href="https://www.notion.so/Cards-197291d3d515808bbfd8f1d66a882c27">
+                        more info
+                    </a>
+                </p>
+            </div>
+        </>
+    );
 };
 
 export default Card;
